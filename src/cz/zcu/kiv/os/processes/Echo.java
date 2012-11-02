6c86e3ea32061950310ab82a3848d0371d6920b3
Jiri Zikmund <jiri.zikmund@socialbakers.com>
2012-11-02 10:49:55 +0100

    Cat process - extends processWithOptions

--- src/cz/zcu/kiv/os/processes/Echo.java
+++ src/cz/zcu/kiv/os/processes/Echo.java
@@ -9,14 +9,52 @@ import java.util.logging.Logger;
  *
  * @author Jiri Zikmund
  */
-public class Echo extends Process {
+public class Echo extends cz.zcu.kiv.os.core.ProcessWithOptions {
+
+	/* @Override */
+	protected String helpText =
+		  "------------------------------"
+		+ "This is help for cat process  "
+		+ "This help is not completed yet"
+		+ "This is help for cat process  "
+		+ "This help is not completed yet"
+		+ "This is help for cat process  "
+		+ "This help is not completed yet"
+		+ "------------------------------";
+	
+	/* @Override */
+	protected String[] definedOptions = {
+		"-n",
+		"-r"
+	};
 	
 	@Override
-	public void run(String[] args) throws Exception {
-		this.getOutputStream().writeLine("start");
-		for (int i = 0; i < 1000000; i++) {
-			this.checkForStop();
-			this.getOutputStream().writeLine(("PID " + (this.pid) + " " + i));
+	protected void runWithOptions(String[] args, String[] names, String[] options) throws Exception {
+		
+		for (int i = 0; i < names.length; i++) {
+			
+			this.writeln("Jmeno souboru " + i + ": '" +  names[i] + "'");
+			
+		}
+		
+	}
+
+	private void writeln(String line) {
+		try {
+			this.stdOut.writeLine(line);
+		} catch (Exception ex) {
+			this.stop("stdOut writeLine exception: " + ex.getMessage());
 		}
 	}
+	
+	private String readln() {
+		try {
+			return this.stdIn.readLine();
+		} catch (Exception ex) {
+			this.stop("stdIn readLine exception: " + ex.getMessage());
+			return null;
+		}
+	}
+	
+	
 }
