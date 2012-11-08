8b7ff5ede7989a94afd46ccb90dc6b40f6a07163
Jiri Zikmund <jiri.zikmund@socialbakers.com>
2012-11-08 22:10:14 +0100

    Echopid process - 1.000.000 iterations, 20 ms sleep

--- src/cz/zcu/kiv/os/processes/Echopid.java
+++ src/cz/zcu/kiv/os/processes/Echopid.java
@@ -17,12 +17,12 @@ public class Echopid extends cz.zcu.kiv.os.core.Process {
 	protected void run(String[] args) throws Exception {
 		try {
 			Thread.sleep(1000);
-			this.stdOut.writeLine("start");
-			for (int i = 0; i < 10; i++) {
-				this.stdOut.writeLine(("PID " + (this.pid)));
-				Thread.sleep(400);
+			this.getOutputStream().writeLine("start");
+			for (int i = 0; i < 1000000; i++) {
+				this.getOutputStream().writeLine(("PID " + (this.pid)) + ", iteration " + i);
+				Thread.sleep(20);
 			}
-			this.stdOut.writeLine("finish");
+			this.getOutputStream().writeLine("finish");
 		} catch (Exception ex) {
 			Logger.getLogger(Echo.class.getName()).log(Level.SEVERE, null, ex);
 		}
