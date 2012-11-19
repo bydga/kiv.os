b6e109a2e7d472fbb87d4370c871373417402539
Jakub Danek <jakub.danek@yoso.fi>
2012-11-19 15:22:27 +0100

    Mkdir implemented.

--- src/cz/zcu/kiv/os/processes/Mkdir.java
+++ src/cz/zcu/kiv/os/processes/Mkdir.java
@@ -1,22 +1,26 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package cz.zcu.kiv.os.processes;
 
 import cz.zcu.kiv.os.core.Core;
 
 /**
+ * Process class which creates all directories given as arguments.
  *
- * @author bydga
+ * Whole directory-tree branch is created if given as argument.
+ *
+ * @author Jakub Danek
  */
 public class Mkdir extends cz.zcu.kiv.os.core.Process{
 
 	@Override
 	protected void run(String[] args) throws Exception {
+		boolean success;
 		
-		if (args.length > 1) {
-			Core.getInstance().getServices().createDirectory(this, args[1]);
+		for(int i = 1; i < args.length; i++) {
+			success = Core.getInstance().getServices().createDirectory(this, args[i]);
+
+			if(!success) {
+				this.getOutputStream().writeLine("Directory: \"" + args[i] + "\" couldnt be created!");
+			}
 		}
 		
 	}
