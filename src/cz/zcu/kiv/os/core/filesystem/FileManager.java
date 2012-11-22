96df5cdd252f2edd19ddffc3370e0fee3f44bd57
Martin Bydzovsky <martin.bydzovsky@socialbakers.com>
2012-11-22 17:39:18 +0100

    windows illegal file-char check added

--- src/cz/zcu/kiv/os/core/filesystem/FileManager.java
+++ src/cz/zcu/kiv/os/core/filesystem/FileManager.java
@@ -19,58 +19,63 @@ import java.util.regex.Matcher;
  */
 public class FileManager {
 
-    public static final String SEPARATOR = System.getProperty("file.separator");
-    
-    private final String rootPath;
+	public static final String SEPARATOR = System.getProperty("file.separator");
+	public static char[] ILLEGAL_FILE_CHARS = new char[]{'/', '\\', '*', '?', ':', '"', '<', '>', '|'};
+	private final String rootPath;
 
-    /**
-     * Default constructor
-     * @param rootPath real path (in the host system) to the simulated system's root dir
-     */
-    public FileManager(String rootPath) {
-        this.rootPath = rootPath;
-    }
+	/**
+	 * Default constructor
+	 *
+	 * @param rootPath real path (in the host system) to the simulated system's root dir
+	 */
+	public FileManager(String rootPath) {
+		this.rootPath = rootPath;
+	}
 
-    /**
-     * Returns device (for read or write) backed by the file on the given path
-     * @param path path to the file
-     * @param workingDir working dir the path is relative to (if the path is relative)
-     * @param mode mode in which the file shall be opened
-     * @return created device
-     * @throws FileNotFoundException if file doesnt exist and couldnt be created
-     */
-    public AbstractDevice openFile(String path, String workingDir, FileMode mode) throws FileNotFoundException {
-        String realPath = resolveRealPath(path, workingDir);
+	/**
+	 * Returns device (for read or write) backed by the file on the given path
+	 *
+	 * @param path path to the file
+	 * @param workingDir working dir the path is relative to (if the path is relative)
+	 * @param mode mode in which the file shall be opened
+	 * @return created device
+	 * @throws FileNotFoundException if file doesnt exist and couldnt be created
+	 */
+	public AbstractDevice openFile(String path, String workingDir, FileMode mode) throws FileNotFoundException {
+		String realPath = resolveRealPath(path, workingDir);
 
-        return prepareDevice(realPath, mode);
-    }
+		return prepareDevice(realPath, mode);
+	}
 
 	/**
 	 * Creates directory/directory branch corresponding to the given path
+	 *
 	 * @param path dirpath
 	 * @param workingDir working dir the path is relative to
 	 * @return
 	 */
-    public boolean createDirectory(String path, String workingDir) {
-        String realPath = resolveRealPath(path, workingDir);
+	public boolean createDirectory(String path, String workingDir) {
+		String realPath = resolveRealPath(path, workingDir);
 
-        return mkDir(realPath);
-    }
+		return mkDir(realPath);
+	}
 
 	/**
 	 * Checks whether directory on the given path exists
+	 *
 	 * @param path dirpath
 	 * @param workingDir working dir the path is relative to
 	 * @return
 	 */
-    public boolean directoryExists(String path, String workingDir) {
-        String realPath = resolveRealPath(path, workingDir);
+	public boolean directoryExists(String path, String workingDir) {
+		String realPath = resolveRealPath(path, workingDir);
 
-        return isDir(realPath);
-    }
+		return isDir(realPath);
+	}
 
 	/**
 	 * Lists all files on the given path
+	 *
 	 * @param path dirpath
 	 * @param workingDir working dir the path is relative to
 	 * @return list of files in the dir given, or filename if path is file, or null otherwise
@@ -80,7 +85,7 @@ public class FileManager {
 
 		File dir = new File(realPath);
 		List<File> l = new ArrayList<File>();
-		if(dir.isDirectory()) {//return dir filelist
+		if (dir.isDirectory()) {//return dir filelist
 			l.add(new File(dir, "."));
 			l.add(new File(dir, ".."));
 			l.addAll(Arrays.asList(dir.listFiles()));
@@ -93,174 +98,182 @@ public class FileManager {
 		}
 	}
 
-    private boolean isDir(String realPath) {
-        File dir = new File(realPath);
-        return dir.isDirectory();
-    }
+	private boolean isDir(String realPath) {
+		File dir = new File(realPath);
+		return dir.isDirectory();
+	}
 
 	/**
 	 * Create dir branch.
+	 *
 	 * @param realPath
 	 * @return
 	 */
-    private boolean mkDir(String realPath) {
-        File dir = new File(realPath);
-        return dir.mkdirs();
-    }
+	private boolean mkDir(String realPath) {
+		File dir = new File(realPath);
+		return dir.mkdirs();
+	}
 
-    /**
-     * Returns the real path (in the host system) to the file on the simulated system's
-     * path (which is given as an argument).
-     * @param path path to the file in the simulated system
-     * @param workingDir directory to which the path is relative to (if related)
-     * @return real path to the file in the host system
-     */
-    public String resolveRealPath(String path, String workingDir) {
-        StringBuilder realPath = new StringBuilder(getRootPath());
+	/**
+	 * Returns the real path (in the host system) to the file on the simulated system's path (which is given as an
+	 * argument).
+	 *
+	 * @param path path to the file in the simulated system
+	 * @param workingDir directory to which the path is relative to (if related)
+	 * @return real path to the file in the host system
+	 */
+	public String resolveRealPath(String path, String workingDir) {
+		StringBuilder realPath = new StringBuilder(getRootPath());
 
-        String innerPath;
-        switch(path.charAt(0)) {
-            case '/': //absolute path
-                innerPath = resolveRelativePath("/", path);
-                break;
-            default: //relative path
-                innerPath = resolveRelativePath(workingDir, path);
-                break;
-        }
+		String innerPath;
+		switch (path.charAt(0)) {
+			case '/': //absolute path
+				innerPath = resolveRelativePath("/", path);
+				break;
+			default: //relative path
+				innerPath = resolveRelativePath(workingDir, path);
+				break;
+		}
 
 		innerPath = innerPath.substring(1);
-        innerPath = substituteFileSeparators(innerPath);
+		innerPath = substituteFileSeparators(innerPath);
 
-        //replace root separator by root path
-        realPath.append(innerPath);
+		//replace root separator by root path
+		realPath.append(innerPath);
 
-        return realPath.toString();
-    }
+		return realPath.toString();
+	}
 
-    private String substituteFileSeparators(String path) {
-        return path.replaceAll("/", Matcher.quoteReplacement(SEPARATOR));
-    }
+	private String substituteFileSeparators(String path) {
+		return path.replaceAll("/", Matcher.quoteReplacement(SEPARATOR));
+	}
 
-    /**
-     * Handles all void directory elements and directory jumps within the path.
-     * @param workingDir directory to which the path is relative to
-     * @param path the path
-     * @return minimal path to the file
-     */
-    public static String resolveRelativePath(String workingDir, String path) {
-        StringBuffer result = new StringBuffer(workingDir);
+	/**
+	 * Handles all void directory elements and directory jumps within the path.
+	 *
+	 * @param workingDir directory to which the path is relative to
+	 * @param path the path
+	 * @return minimal path to the file
+	 */
+	public static String resolveRelativePath(String workingDir, String path) {
+		StringBuffer result = new StringBuffer(workingDir);
 
-        if(!workingDir.endsWith("/")) {
-            result.append("/"); //end working dir with dir separator
-        }
+		if (!workingDir.endsWith("/")) {
+			result.append("/"); //end working dir with dir separator
+		}
 
-        StringBuilder tmp = new StringBuilder();
-        char character;
+		StringBuilder tmp = new StringBuilder();
+		char character;
 
-        for(int i = 0; i < path.length(); i++) {
-            character = path.charAt(i);
-            switch(character) {
-                case '/': //dirname end
-                    if(tmp.length() > 0) {//any dirname, otherwise just skip
-                        resolveFileName(result, tmp.toString(), true);
-                        tmp.delete(0, tmp.length()); //clean temp buffer
-                    }
-                    break;
-                default:
-                    tmp.append(character);
-                    break;
-            }
-        }
+		for (int i = 0; i < path.length(); i++) {
+			character = path.charAt(i);
+			switch (character) {
+				case '/': //dirname end
+					if (tmp.length() > 0) {//any dirname, otherwise just skip
+						resolveFileName(result, tmp.toString(), true);
+						tmp.delete(0, tmp.length()); //clean temp buffer
+					}
+					break;
+				default:
+					tmp.append(character);
+					break;
+			}
+		}
 
-        if(tmp.length() > 0) {//filename
-            resolveFileName(result, tmp.toString(), false);
-        }
+		if (tmp.length() > 0) {//filename
+			resolveFileName(result, tmp.toString(), false);
+		}
 
-        return result.toString();
-    }
+		return result.toString();
+	}
 
-    /**
-     * Handles special directory names (level up "..", same direcotry "." etc.)
-     * @param result buffer to which the changes are saved
-     * @param dirname dirname to be processed
-     */
-    private static void resolveFileName(StringBuffer result, String dirname, boolean apendDirEnd) {
-        if(dirname.equals(".")) {
-            //do nothing, same directory
-        } else if (dirname.equals("..")) {
-            levelUpDir(result);
-        } else {
-            result.append(dirname);
-            if(apendDirEnd) {
-                result.append("/");
-            }
-        }
-    }
+	/**
+	 * Handles special directory names (level up "..", same direcotry "." etc.)
+	 *
+	 * @param result buffer to which the changes are saved
+	 * @param dirname dirname to be processed
+	 */
+	private static void resolveFileName(StringBuffer result, String dirname, boolean apendDirEnd) {
+		if (dirname.equals(".")) {
+			//do nothing, same directory
+		} else if (dirname.equals("..")) {
+			levelUpDir(result);
+		} else {
+			result.append(dirname);
+			if (apendDirEnd) {
+				result.append("/");
+			}
+		}
+	}
 
-    /**
-     * Moves one directory up the path, unless its root already
-     * @param path
-     */
-    private static void levelUpDir(StringBuffer path) {
-        if(path.length() == 1) { //root only
-            return;
-        }
-        int index = path.substring(0, path.length() - 1).lastIndexOf("/"); //last "not-final" directory separator
-        path.delete(index + 1, path.length()); //delete last directory from the path
-    }
+	/**
+	 * Moves one directory up the path, unless its root already
+	 *
+	 * @param path
+	 */
+	private static void levelUpDir(StringBuffer path) {
+		if (path.length() == 1) { //root only
+			return;
+		}
+		int index = path.substring(0, path.length() - 1).lastIndexOf("/"); //last "not-final" directory separator
+		path.delete(index + 1, path.length()); //delete last directory from the path
+	}
 
-    private String getRootPath() {
-        return rootPath;
-    }
+	private String getRootPath() {
+		return rootPath;
+	}
 
-    /**
-     * Opens file on the given path for reading
-     * @param path real path in the host system
-     * @return
-     * @throws FileNotFoundException
-     */
-    private FileInputStream openFileForRead(String path) throws FileNotFoundException {
-        File file = new File(path);
-        //TODO set read only?
-        return new FileInputStream(file);
-    }
+	/**
+	 * Opens file on the given path for reading
+	 *
+	 * @param path real path in the host system
+	 * @return
+	 * @throws FileNotFoundException
+	 */
+	private FileInputStream openFileForRead(String path) throws FileNotFoundException {
+		File file = new File(path);
+		//TODO set read only?
+		return new FileInputStream(file);
+	}
 
-    /**
-     * Opens file on the given path for writing
-     * @param path real path in the host system
-     * @param append append?
-     * @return
-     * @throws FileNotFoundException
-     */
-    private FileOutputStream openFileForWrite(String path, boolean append) throws FileNotFoundException {
-        File file = new File(path);
-        //TODO set writable?
-        return new FileOutputStream(file, append);
-    }
+	/**
+	 * Opens file on the given path for writing
+	 *
+	 * @param path real path in the host system
+	 * @param append append?
+	 * @return
+	 * @throws FileNotFoundException
+	 */
+	private FileOutputStream openFileForWrite(String path, boolean append) throws FileNotFoundException {
+		File file = new File(path);
+		//TODO set writable?
+		return new FileOutputStream(file, append);
+	}
 
-    /**
-     * Convenience class for preparing device for reading/writing from/to the file on the given path.
-     * @param path path in the simulated system
-     * @param mode open mode
-     * @return device
-     * @throws FileNotFoundException
-     */
-    private AbstractDevice prepareDevice(String path, FileMode mode) throws FileNotFoundException {
-        AbstractDevice dev;
-        switch(mode) {
-            case READ:
-                dev = new FileInputDevice(openFileForRead(path), path);
-                break;
-            case WRITE:
-                dev = new FileOutputDevice(openFileForWrite(path, false), path);
-                break;
-            case APPEND:
-                dev = new FileOutputDevice(openFileForWrite(path, true), path);
-                break;
-            default:
-                //TODO better exception
-                throw new RuntimeException();
-        }
-        return dev;
-    }
+	/**
+	 * Convenience class for preparing device for reading/writing from/to the file on the given path.
+	 *
+	 * @param path path in the simulated system
+	 * @param mode open mode
+	 * @return device
+	 * @throws FileNotFoundException
+	 */
+	private AbstractDevice prepareDevice(String path, FileMode mode) throws FileNotFoundException {
+		AbstractDevice dev;
+		switch (mode) {
+			case READ:
+				dev = new FileInputDevice(openFileForRead(path), path);
+				break;
+			case WRITE:
+				dev = new FileOutputDevice(openFileForWrite(path, false), path);
+				break;
+			case APPEND:
+				dev = new FileOutputDevice(openFileForWrite(path, true), path);
+				break;
+			default:
+				//TODO better exception
+				throw new RuntimeException();
+		}
+		return dev;
+	}
 }
--- src/cz/zcu/kiv/os/processes/Login.java
+++ src/cz/zcu/kiv/os/processes/Login.java
@@ -4,10 +4,12 @@
  */
 package cz.zcu.kiv.os.processes;
 
+import com.sun.xml.internal.ws.util.StringUtils;
 import cz.zcu.kiv.os.core.Core;
 import cz.zcu.kiv.os.core.ProcessGroup;
 import cz.zcu.kiv.os.core.ProcessProperties;
 import cz.zcu.kiv.os.core.Process;
+import cz.zcu.kiv.os.core.filesystem.FileManager;
 
 /**
  * Process that handles login procedure. It is usually run by init. After successfull login creates new Shell (on foreground).
@@ -15,13 +17,28 @@ import cz.zcu.kiv.os.core.Process;
  */
 public class Login extends Process {
 
+	private boolean containsAny(String str, char[] searchChars) {
+      if (str == null || str.length() == 0 || searchChars == null || searchChars.length == 0) {
+          return false;
+      }
+      for (int i = 0; i < str.length(); i++) {
+          char ch = str.charAt(i);
+          for (int j = 0; j < searchChars.length; j++) {
+              if (searchChars[j] == ch) {
+                  return true;
+              }
+          }
+      }
+      return false;
+  }
+	
 	@Override
 	protected void run(String[] args) throws Exception {
 
 		this.getOutputStream().writeLine("Enter login: ");
 		String login = this.getInputStream().readLine();
 
-		while (login.equals("")) {
+		while (login.equals("") || this.containsAny(login, FileManager.ILLEGAL_FILE_CHARS)) {
 			this.getOutputStream().writeLine("Invalid login name. Enter login:");
 			login = this.getInputStream().readLine();
 		}
